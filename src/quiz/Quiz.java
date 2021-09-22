c615dd338ba9e62b59781ef6210b6dfed033cee1
JDCRecomendable <JDCRecomendable@protonmail.ch>
2021-09-23 00:55:35 +1200

    Add Quiz class
    Used to fetch questions for showing to the user.

---
+++ src/quiz/Quiz.java
@@ -0,0 +1,31 @@
+package quiz;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class Quiz {
+	public ArrayList<String> words;
+	private int maximumNumberOfQuestions;
+	private int questionNumber;
+
+	public Quiz(Collection<? extends String> words, int maximumNumberOfQuestions) {
+		this.words.addAll(words);
+		this.maximumNumberOfQuestions = maximumNumberOfQuestions;
+		questionNumber = 0;
+	}
+
+	public Boolean hasNextQuestion() {
+		return (questionNumber >= maximumNumberOfQuestions);
+	}
+
+	public Question getNextQuestion() {
+		if (hasNextQuestion()) {
+			return null;
+		} else {
+			String word = words.get(questionNumber);
+			Question question = new Question(word);
+			questionNumber++;
+			return question;
+		}
+	}
+}
