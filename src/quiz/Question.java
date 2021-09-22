95bf51dbbb9b57dc18b90813965ab35c9c5b0fd1
JDCRecomendable <JDCRecomendable@protonmail.ch>
2021-09-23 00:23:27 +1200

    Add Question class
    Class used to store a word during a question session in the quiz.
    Whenever the user attempts to spell the word, we can check their answer
    using the checkAnswer method in this class.  Depending on the user's
    answer, the appropriate AnswerStatus is returned.

---
+++ src/quiz/Question.java
@@ -0,0 +1,38 @@
+package quiz;
+
+public class Question {
+	private String word;
+	private int attemptNumber;
+
+	public Question(String word) {
+		this.word = word;
+		attemptNumber = 1;
+	}
+
+	public AnswerStatus checkAnswer(String answer) {
+		if (word.equals(answer)) {
+			return determineAnswerStatusIfCorrect();
+		} else {
+			attemptNumber++;
+			return determineAnswerStatusIfIncorrect();
+		}
+	}
+
+	private AnswerStatus determineAnswerStatusIfCorrect() {
+		switch (attemptNumber) {
+		case (2):
+			return AnswerStatus.FAULTED;
+		default:
+			return AnswerStatus.MASTERED;
+		}
+	}
+
+	private AnswerStatus determineAnswerStatusIfIncorrect() {
+		switch (attemptNumber) {
+		case (3):
+			return AnswerStatus.FAILED;
+		default:
+			return AnswerStatus.INCORRECT;
+		}
+	}
+}
