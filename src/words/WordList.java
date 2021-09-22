1a34c5ac0d00a3cb378135946d8b6570c41a2060
JDCRecomendable <JDCRecomendable@protonmail.ch>
2021-09-22 23:35:23 +1200

    Add WordList class
    WordList class is used to store words that belong to a particular list

---
+++ src/words/WordList.java
@@ -0,0 +1,48 @@
+package words;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Random;
+
+public class WordList {
+	private HashSet<String> wordList;
+
+	public WordList() {
+		wordList = new HashSet<String>();
+	}
+
+	public void addWord(String word) {
+		wordList.add(word);
+	}
+
+	public HashSet<String> getRandomWords(int count) {
+		HashSet<String> selection = new HashSet<>();
+		count = Integer.min(count, wordList.size());
+		while (selection.size() != count) {
+			selection.add(getRandomWord());
+		}
+		return selection;
+	}
+
+	private String getRandomWord() {
+		String selection = null;
+		int selectedIndex = getRandomInteger(wordList.size());
+		int currentIndex = 0;
+
+		Iterator<? extends String> iterator = wordList.iterator();
+		while (iterator.hasNext()) {
+			selection = iterator.next();
+			if (currentIndex == selectedIndex) {
+				return selection;
+			}
+			currentIndex++;
+		}
+
+		return selection;
+	}
+
+	private int getRandomInteger(int bound) {
+		Random random = new Random();
+		return random.nextInt(bound);
+	}
+}
