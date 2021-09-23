eceb9dd7ef6c4273ac8be81c01bd9a0226beb79b
ykim273 <ykim273@github.com>
2021-09-23 17:18:58 +1200

    New file: create parent Controller class with switch scenes method implemented
    The Contoller.java class will be the parent that other controllers will be extended from.
    It has the switch scene method that intakes ActionEvent and the string of next scene.

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,5 +1,31 @@
 package controller;
 
-public class Controller {
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+
+public class Controller {
+	
+	private Stage stage;
+	private Scene scene;
+	private Parent root;
+
+	public void switchScenes(ActionEvent event, String nextScene) {
+		try {
+			root = FXMLLoader.load(getClass().getResource(nextScene));
+			stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+			scene = new Scene(root);
+			stage.setScene(scene);
+			stage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
 }
