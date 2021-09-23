fff518f1ac0cc3f8e2d1c6bccbffc1410f60026c
ykim273 <ykim273@github.com>
2021-09-23 18:16:14 +1200

    New Controller: TopicsView - initliased buttons on Topic List scene
    The button variables initialised on the controller.
    fx:id assign to all toggle buttons that switch on and off each topic list to be added or not.

---
+++ src/controller/TopicsView.java
@@ -0,0 +1,36 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class TopicsView {
+
+	@FXML
+	private Button colour;
+	@FXML
+	private BButton weather;
+	@FXML
+	private Button dayOfWeekOne;
+	@FXML
+	private Button dayOfWeekTwo;
+	@FXML
+	private Button monthOfYearOne;
+	@FXML
+	private Button monthOfYearTwo;
+	@FXML
+	private Button baby;
+	@FXML
+	private Button work;
+	@FXML
+	private Button feeling;
+	@FXML
+	private Button compassPoint;
+	@FXML
+	private Button university;
+	@FXML
+	private Button software;
+	@FXML
+	private Button start;
+	@FXML
+	private Button back;
+}
