db5e92639a2fb3382464809877f5cd62a12a9a7e
ykim273 <ykim273@github.com>
2021-09-25 02:13:59 +1200

    Fix and Modified: The TopicsView controller for TopicList scene
    The HashMap method used previously is deleted - hence the ToggleButton class not used anymore.
    Instead changed all Buttons to ToggleButtons with -fx-graphic set to offButtonImage
    when button is selected the graphic changes to OnButtonImage.

--- src/controller/TopicsView.java
+++ src/controller/TopicsView.java
@@ -6,98 +6,37 @@ import java.util.Map;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ToggleButton;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 public class TopicsView extends Controller {
 
 	@FXML
-	private ToggleButton colour;
+	private ToggleButton colourButton,weatherButton,dayOneButton,dayTwoButton,monthOneButton,monthTwoButton,
+						 babyButton,workButton,feelingButton,compassPointButton,universityButton,softwareButton;
 	@FXML
-	private ToggleButton weather;
+	private Button start,back;
 	@FXML
-	private ToggleButton dayOfWeekOne;
-	@FXML
-	private ToggleButton dayOfWeekTwo;
-	@FXML
-	private ToggleButton monthOfYearOne;
-	@FXML
-	private ToggleButton monthOfYearTwo;
-	@FXML
-	private ToggleButton baby;
-	@FXML
-	private ToggleButton work;
-	@FXML
-	private ToggleButton feeling;
-	@FXML
-	private ToggleButton compassPoint;
-	@FXML
-	private ToggleButton university;
-	@FXML
-	private Button software;
-	@FXML
-	private Button start;
-	@FXML
-	private Button back;
-	@FXML
-	private ImageView colourSwitch;
-	@FXML
-	private ImageView weatherSwitch;
-	@FXML
-	private ImageView dayOfWeekOneSwitch;
-	@FXML
-	private ImageView dayOfWeekTwoSwitch;
-	@FXML
-	private ImageView monthOfYearOneSwitch;
-	@FXML
-	private ImageView monthOfYearTwoSwitch;
-	@FXML
-	private ImageView babySwitch;
-	@FXML
-	private ImageView workSwitch;
-	@FXML
-	private ImageView feelingSwitch;
-	@FXML
-	private ImageView compassPointSwitch;
-	@FXML
-	private ImageView universitySwitch;
-	@FXML
-	private ImageView softwareSWitch;
-	
-	Image onButtonImage = new Image(getClass().getResourceAsStream("Toggle_Button_On.png"));
-	Image offButtonImage = new Image(getClass().getResourceAsStream("Toggle_Button_Off.png"));
-	Map <Button, ImageView> buttonsMap = new HashMap<>();
-	
-	public void initialize() {
-		buttonsMap.put(colour, colourSwitch);
-		buttonsMap.put(weather, weatherSwitch);
-		buttonsMap.put(dayOfWeekOne, dayOfWeekOneSwitch);
-		buttonsMap.put(dayOfWeekTwo, dayOfWeekTwoSwitch);
-		buttonsMap.put(monthOfYearOne, monthOfYearOneSwitch);
-		buttonsMap.put(baby, babySwitch);
-		buttonsMap.put(work, workSwitch);
-		buttonsMap.put(feeling, feelingSwitch);
-		buttonsMap.put(compassPoint, compassPointSwitch);
-		buttonsMap.put(university, universitySwitch);
-		buttonsMap.put(software, softwareSWitch);
-	}
+	private Label startWarning;
+
+	Image onButtonImage = new Image(getClass().getResourceAsStream("../resources/Toggle_Button_On.png"));
+	Image offButtonImage = new Image(getClass().getResourceAsStream("../resources/Toggle_Button_Off.png"));
 	
 	public void toggleTopic(ActionEvent event){
 		ToggleButton selectedTopic = (ToggleButton) event.getSource();
-		ImageView toggleSwitchImage = buttonsMap.get(selectedTopic);
-		selectedTopic.turnOnOrOff();
-		boolean topicIsOn = selectedTopic.getState();
-		if (topicIsOn) {
-			toggleSwitchImage.setImage(onButtonImage);
-		}
-		else {
-			toggleSwitchImage.setImage(offButtonImage);
+		String id = selectedTopic.getId();
+		if (selectedTopic.isSelected()) {
+			selectedTopic.setGraphic(new ImageView(onButtonImage));
+			//addList(id);
+		} else if (!selectedTopic.isSelected()){
+			selectedTopic.setGraphic(new ImageView(offButtonImage));
+			//removeList(id);
 		}
 	}
 	
 	public void startGame(ActionEvent start) {
-		
 		switchScene(start, "GameScreen.fxml");
-	}
-	
+	}	
 }
