6e3b6abc2f805b76ff4c32d0594fa703a97fc82b
ykim273 <ykim273@github.com>
2021-09-25 02:45:12 +1200

    Implemented the Start Game method to only start if at least one button is toggled.
    Added initialise method that adds all toggle buttons in to an array so they can later be
    examined one by one to count the total number of buttons selected.
    If there is none, a warning message is displayed. Is there is at least one, it proceeds to the
    games screen.

--- src/controller/TopicsView.java
+++ src/controller/TopicsView.java
@@ -1,5 +1,6 @@
 package controller;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -8,6 +9,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
@@ -23,6 +25,27 @@ public class TopicsView extends Controller {
 
 	Image onButtonImage = new Image(getClass().getResourceAsStream("../resources/Toggle_Button_On.png"));
 	Image offButtonImage = new Image(getClass().getResourceAsStream("../resources/Toggle_Button_Off.png"));
+	private ArrayList<ToggleButton> toggles = new ArrayList<ToggleButton>();
+	
+	@FXML
+	private void initialize() {
+		toggles.add(colourButton);
+		toggles.add(weatherButton);
+		toggles.add(dayOneButton);
+		toggles.add(dayTwoButton);
+		toggles.add(monthOneButton);
+		toggles.add(monthTwoButton);
+		toggles.add(babyButton);
+		toggles.add(workButton);
+		toggles.add(feelingButton);
+		toggles.add(compassPointButton);
+		toggles.add(universityButton);
+		toggles.add(softwareButton);
+		
+		for (ToggleButton topic : toggles) {
+			topic.getUserData();
+		}
+	}
 	
 	public void toggleTopic(ActionEvent event){
 		ToggleButton selectedTopic = (ToggleButton) event.getSource();
@@ -37,6 +60,18 @@ public class TopicsView extends Controller {
 	}
 	
 	public void startGame(ActionEvent start) {
-		switchScene(start, "GameScreen.fxml");
+		int numTopics = 0;
+		for(ToggleButton topic : toggles) {
+			topic.setUserData(topic.selectedProperty());
+			if (topic.isSelected()) {
+				numTopics++;
+			}
+		}
+		if (numTopics == 0) {
+			startWarning.setVisible(true);
+		} else {
+			switchScene(start, "ResultScreen.fxml");
+		}
 	}	
+	
 }
