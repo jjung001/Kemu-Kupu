0e60d2a804895d5a475aa832a2f2d4579baa2e8c
JDCRecomendable <JDCRecomendable@protonmail.ch>
2021-09-26 03:42:18 +1300

    Store words in ScoreTracker class
    Will be needed to show in Results scene.

--- src/game/ScoreTracker.java
+++ src/game/ScoreTracker.java
@@ -2,6 +2,7 @@ package game;
 
 public class ScoreTracker {
 	private int[] scores;
+	private String[] words;
 	private int maximumNumberOfQuestions;
 
 	public ScoreTracker(int maximumNumberOfQuestions) {
@@ -9,12 +10,17 @@ public class ScoreTracker {
 		scores = new int[maximumNumberOfQuestions];
 	}
 
-	public void updateScore(int questionNumber, int score) {
+	public void update(int questionNumber, int score, String word) {
 		scores[questionNumber - 1] = score;
+		words[questionNumber - 1] = word;
 	}
 
 	public int getScore(int questionNumber) {
 		return scores[questionNumber - 1];
+	}
+
+	public String getWord(int questionNumber) {
+		return words[questionNumber - 1];
 	}
 
 	public int getTotalScore() {
