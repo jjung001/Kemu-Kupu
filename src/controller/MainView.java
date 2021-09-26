2e4cd34fa14a5ef16c38a3a7fa5043d67b7ed8d3
ykim273 <ykim273@github.com>
2021-09-27 04:38:41 +1300

    Comment on MainView.java controller class

--- src/controller/MainView.java
+++ src/controller/MainView.java
@@ -4,6 +4,11 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 
+/**
+ * This is a controller class for Main Menu.
+ * @author Julie Kim
+ *
+ */
 public class MainView extends Controller {
 
 	@FXML
@@ -11,10 +16,18 @@ public class MainView extends Controller {
 	@FXML
 	private Button quitButton;
 
+	/**
+	 * Starts game module by switching scene to the topic list scene.
+	 * @param play	ActionEvent from play game button 
+	 */
 	public void playGame(ActionEvent play) {
 		switchScene(play, "TopicList.fxml");
 	}
 
+	/**
+	 * Exits and shuts down window when the quit button is pressed. 
+	 * @param quit	ActionEvent from quit button
+	 */
 	public void quitGame(ActionEvent quit) {
 		System.exit(0);
 	}
