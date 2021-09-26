04397cf5da1504c83b7aec25304501cab6ba112d
JDCRecomendable <JDCRecomendable@protonmail.ch>
2021-09-27 03:23:16 +1300

    Do not show alert box when quitting from main menu

--- src/controller/MainView.java
+++ src/controller/MainView.java
@@ -1,26 +1,22 @@
 package controller;
 
-import application.AlertBox;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 
-
-public class MainView extends Controller{
+public class MainView extends Controller {
 
 	@FXML
 	private Button gameButton;
 	@FXML
 	private Button quitButton;
-	
+
 	public void playGame(ActionEvent play) {
 		switchScene(play, "TopicList.fxml");
 	}
-	
+
 	public void quitGame(ActionEvent quit) {
-		if (AlertBox.display("Are you sure you want to quit?", "The window will close immediately.")) {
-			System.exit(0);
-		}
+		System.exit(0);
 	}
-	
+
 }
