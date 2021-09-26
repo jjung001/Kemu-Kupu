1b5c7c434723568aa14ecabe2d2090e602330be1
jjun273 <jjun273@aucklanduni.ac.nz>
2021-09-26 23:50:35 +1300

    Implement two different festival methods to call using english and maori voice

--- src/application/Festival.java
+++ src/application/Festival.java
@@ -30,4 +30,29 @@ public class Festival {
 			alert.showAndWait();
 		}
 	}
+	
+	public static void festivalEnglish(double speed, String word) {
+
+		String cmd = "echo \"(voice_akl_nz_jdt_diphone) (Parameter.set 'Duration_Stretch " + speed + ") (SayText \\\"" + word + "\\\")\" | festival --pipe";
+		ProcessBuilder builder = new ProcessBuilder("/bin/bash", "-c", cmd);
+		try {
+			Process process = builder.start();
+			process.waitFor();
+			process.destroy();
+		} catch (IOException e) {
+			e.printStackTrace();
+			Alert alert = new Alert(Alert.AlertType.ERROR);
+			alert.setTitle("Spelling Quiz Error");
+			alert.setHeaderText("IOException Caught, Using Festival");
+			alert.setContentText("Please contact the developer.");
+			alert.showAndWait();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+			Alert alert = new Alert(Alert.AlertType.ERROR);
+			alert.setTitle("Spelling Quiz Error");
+			alert.setHeaderText("Operation Interrupted");
+			alert.setContentText("Please contact the developer.");
+			alert.showAndWait();
+		}
+	}
 }
