1385b9b34b37d7320f8e52b428e7a11937f08850
jjun273 <jjun273@aucklanduni.ac.nz>
2021-10-19 06:39:56 +1300

    Initialize cash from stored cash file
    Make right and left buttons set to not visible as there are only six items.
    Save cash into cash file.
    Method for buyWater checks if there is enough cash, and withdraws.

--- src/controller/MyTreeShopController.java
+++ src/controller/MyTreeShopController.java
@@ -1,9 +1,17 @@
 package controller;
 
+import java.time.OffsetDateTime;
+
+import application.Cash;
+import application.FileSaveLocations;
+import fileio.CashIO;
+import fileio.ItemStockIO;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import tree.Item;
+import tree.ItemStock;
 
 public class MyTreeShopController extends Controller {
 	
@@ -27,9 +35,38 @@ public class MyTreeShopController extends Controller {
     private Button btnRight;
 	@FXML
     private Button back;
+	
+	private OffsetDateTime offSetDateTime;
+	private ItemStockIO itemStockIO;
+	private CashIO cashIO;
+	private Cash treeMoney;
+	private ItemStock stock;
+	private Item item;
 
     @FXML
     void quitShop(ActionEvent event) {
 		switchScene(event, "MyTree.fxml");
+    }
+    
+    @FXML
+    void buyWater(ActionEvent event) {
+//    	offSetDateTime = OffsetDateTime.now(); 
+    	itemStockIO = new ItemStockIO(FileSaveLocations.INVENTORY);
+    	treeMoney.setCash(treeMoney.getCash());
+    	if (treeMoney.hasEnoughFunds(200)) {
+    		treeMoney.withdraw(200);
+    	} 
+    	String money = String.valueOf(treeMoney.getCash());
+    	moneyLabel.setText(money);   
+    	cashIO.saveCash(treeMoney);
+    }
+    
+    public void initialize() {
+    	btnRight.setVisible(false);
+    	btnLeft.setVisible(false);
+    	cashIO = new CashIO(FileSaveLocations.CASH);
+    	treeMoney = cashIO.loadCash();
+    	String money = String.valueOf(treeMoney.getCash());
+    	moneyLabel.setText(money);
     }
 }
