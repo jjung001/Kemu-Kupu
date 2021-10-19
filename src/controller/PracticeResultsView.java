056ab28a6247223f68c73849bf5cdf944797a1d6
ykim273 <ykim273@github.com>
2021-10-19 13:34:54 +1300

    Implemented the statistic view display on practice results screen

--- src/controller/PracticeResultsView.java
+++ src/controller/PracticeResultsView.java
@@ -1,11 +1,13 @@
 package controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import quiz.AnswerAttemptTracker;
 import quiz.AnswerStatusTracker;
+import quiz.ResultsModel;
 import quiz.ScoreTracker;
 
 public class PracticeResultsView {
@@ -19,8 +21,33 @@ public class PracticeResultsView {
 	@FXML
 	private ImageView wordOneStatus, wordTwoStatus, wordThreeStatus, wordFourStatus, wordFiveStatus;
 	
+	ResultsModel resultsModel; 
+	
 	public void setUp(ScoreTracker scoreTracker, AnswerAttemptTracker answerAttemptTracker, AnswerStatusTracker answerStatusTracker) {
+		resultsModel = new ResultsModel(scoreTracker, answerAttemptTracker, answerStatusTracker);
 		
+		setScreenOne(scoreTracker, resultsModel);
+		
+	}
+	
+	private void setScreenOne(ScoreTracker scoreTracker, ResultsModel resultModel) {
+		wordOne.setText(scoreTracker.getWord(1));
+		wordTwo.setText(scoreTracker.getWord(2));
+		wordThree.setText(scoreTracker.getWord(3));
+		wordFour.setText(scoreTracker.getWord(4));
+		wordFive.setText(scoreTracker.getWord(5));
+		
+		wordOneAttemptLabel.setText(resultsModel.getAttempt(1));
+		wordTwoAttemptLabel.setText(resultsModel.getAttempt(2));
+		wordThreeAttemptLabel.setText(resultsModel.getAttempt(3));
+		wordFourAttemptLabel.setText(resultsModel.getAttempt(4));
+		wordFiveAttemptLabel.setText(resultsModel.getAttempt(5));
+		
+		wordOneStatus.setImage(resultsModel.getStatusImage(1));
+		wordTwoStatus.setImage(resultsModel.getStatusImage(2));
+		wordThreeStatus.setImage(resultsModel.getStatusImage(3));
+		wordFourStatus.setImage(resultsModel.getStatusImage(4));
+		wordFiveStatus.setImage(resultsModel.getStatusImage(5));
 	}
 
 }
