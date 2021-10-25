3a6e2146e97d9edb30587046fe914a0bb2109f16
JDCRecomendable <JDCRecomendable@protonmail.ch>
2021-10-25 20:16:31 +1300

    Add file save location for highest quiz earnings

--- src/application/FileSaveLocations.java
+++ src/application/FileSaveLocations.java
@@ -5,6 +5,7 @@ public final class FileSaveLocations {
 	public static final String SHOP = ".data/shop";
 	public static final String INVENTORY = ".data/inventory";
 	public static final String STATISTICS = ".data/statistics";
+	public static final String QUIZ_EARNINGS = ".data/quizearnings";
 	public static final String TREE_FEED_EVENT = ".data/shop";
 	public static final String TREE_STATISTICS = ".data/tree";
 }
