5d588b25821222505df7a0fd64a591c1ecf99f08
JDCRecomendable <JDCRecomendable@protonmail.ch>
2021-10-25 20:16:12 +1300

    Add ability to record and get highest earnings per quiz

---
+++ src/fileio/HighestEarningsIO.java
@@ -0,0 +1,46 @@
+package fileio;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class HighestEarningsIO extends FileIO {
+
+	public HighestEarningsIO(String filepath) {
+		super(filepath);
+	}
+
+	public void recordQuizEarning(int totalScoreForQuiz) {
+		ArrayList<Integer> topQuizScores = getExistingTopQuizEarnings();
+		topQuizScores.add(totalScoreForQuiz);
+		Collections.sort(topQuizScores);
+		topQuizScores.remove(0);
+		Collections.reverse(topQuizScores);
+		saveTopQuizEarnings(topQuizScores);
+	}
+
+	private ArrayList<Integer> getExistingTopQuizEarnings() {
+		ArrayList<String> existingTotalScoreStrings = readLines();
+		ArrayList<Integer> existingTopQuizScores = new ArrayList<Integer>();
+		int numberOfScores = existingTotalScoreStrings.size();
+		for (int i = 0; i < numberOfScores; i++) {
+			String totalScoreString = existingTotalScoreStrings.get(i);
+			int totalScore = Integer.valueOf(totalScoreString);
+			existingTopQuizScores.add(totalScore);
+		}
+		return existingTopQuizScores;
+	}
+
+	private void saveTopQuizEarnings(ArrayList<Integer> sortedTopQuizScores) {
+		clearFile();
+		int numberOfScores = sortedTopQuizScores.size();
+		for (int i = 0; i < numberOfScores; i++) {
+			int score = sortedTopQuizScores.get(i);
+			String scoreString = String.valueOf(score);
+			appendLine(scoreString);
+		}
+	}
+
+	public ArrayList<String> getHighestQuizEarningsAsStrings() {
+		return readLines();
+	}
+}
